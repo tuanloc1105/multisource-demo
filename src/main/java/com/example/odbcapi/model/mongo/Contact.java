8cf446de951edabf241bc1ecc0c5ce95a4608ef6
Lộc Võ Tuấn <tuanloc1105@gmail.com>
2022-05-27 10:07:45 +0700

    update code

---
+++ src/main/java/com/example/odbcapi/model/mongo/Contact.java
@@ -0,0 +1,13 @@
+package com.example.odbcapi.model.mongo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "contact")
+public class Contact {
+}
