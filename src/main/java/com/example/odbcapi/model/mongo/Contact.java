42194ce68a801c7da969e71c4b736c74bfcd0cb2
Lộc Võ Tuấn <tuanloc1105@gmail.com>
2022-05-27 10:09:37 +0700

    Update Contact.java

--- src/main/java/com/example/odbcapi/model/mongo/Contact.java
+++ src/main/java/com/example/odbcapi/model/mongo/Contact.java
@@ -4,10 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.Id;
+import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Document(collection = "contact")
 public class Contact {
+
+    @Id
+    private UUID id;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "age")
+    private Integer age;
+
 }
