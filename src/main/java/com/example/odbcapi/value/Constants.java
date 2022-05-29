14f8e5a46ba3961cfbe952dc32eb960b3798f8a7
Lộc Võ Tuấn <tuanloc1105@gmail.com>
2022-05-29 21:00:50 +0700

    Update Constants.java

--- src/main/java/com/example/odbcapi/value/Constants.java
+++ src/main/java/com/example/odbcapi/value/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String[] PACKAGE_ENTITIES_MYSQL = new String[]{"com.example.odbcapi.model.mysql"};
 
-    public static final String JPA_UNIT_NAME_POSTGRES = "PERSITENCE_UNIT_NAME_POSTGRES";
-    public static final String JPA_UNIT_NAME_MYSQL = "PERSITENCE_UNIT_NAME_MYSQL";
+    public static final String JPA_UNIT_NAME_POSTGRES = "PERSISTENCE_UNIT_NAME_POSTGRES";
+    public static final String JPA_UNIT_NAME_MYSQL = "PERSISTENCE_UNIT_NAME_MYSQL";
 
 }
