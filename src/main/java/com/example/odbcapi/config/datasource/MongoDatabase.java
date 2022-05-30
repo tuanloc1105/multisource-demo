1be54b4c6af453edf0aa2ed6c1a4c78839255b0a
Lộc Võ Tuấn <tuanloc1105@gmail.com>
2022-05-30 17:27:24 +0700

    update code

--- src/main/java/com/example/odbcapi/config/datasource/MongoDatabase.java
+++ src/main/java/com/example/odbcapi/config/datasource/MongoDatabase.java
@@ -1,34 +1,53 @@
 package com.example.odbcapi.config.datasource;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.bson.UuidRepresentation;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoRepositories(
+        basePackages = "com.example.odbcapi.repository.mongo"
+)
 public class MongoDatabase {
 
     @Value("${spring.datasource.mongodb.authentication-database}")
     private String mongoAuthDb;
-    
+
     @Value("${spring.datasource.mongodb.database}")
     private String mongoDb;
-    
+
     @Value("${spring.datasource.mongodb.port}")
     private Integer mongoPort;
-    
+
     @Value("${spring.datasource.mongodb.host}")
     private String mongoHost;
-    
 
-    public MongoProperties getPrimary() {
-        MongoProperties mongoProperties = new MongoProperties();
-        mongoProperties.setUuidRepresentation(UuidRepresentation.JAVA_LEGACY);
-        mongoProperties.setAuthenticationDatabase(this.mongoAuthDb);
-        mongoProperties.setDatabase(this.mongoDb);
-        mongoProperties.setHost(this.mongoHost);
-        mongoProperties.setPort(this.mongoPort);
-        return new MongoProperties();
+    @Value("${spring.datasource.mongodb.uri}")
+    private String mongoUri;
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        return new SimpleMongoClientDatabaseFactory(this.mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(this.mongoDatabaseFactory());
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(MongoClientSettings.builder()
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .build());
     }
 
 }
--- src/main/java/com/example/odbcapi/service/ContactServiceMongo.java
+++ src/main/java/com/example/odbcapi/service/ContactServiceMongo.java
@@ -6,12 +6,16 @@ import com.example.odbcapi.model.mongo.Contact;
 import com.example.odbcapi.repository.mongo.ContactMongoRepository;
 import com.example.odbcapi.value.ProcessStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ContactServiceMongo {
 
+    private final ApplicationContext context;
     private final ContactMongoRepository contactMongoRepository;
 
     public ProcessResponse addNew(Param param) {
